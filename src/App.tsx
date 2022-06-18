3ec5bb18db466bd7685434a529f0e74502a70c62
Nils Rugsveen <nirugsveen@gmail.com>
2022-06-18 14:22:34 +0200

    Set correct name in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Example } from "./sketches/Example";
+import { Flowfields } from "./solutions/Flowfields";
 
 function App() {
   return (
     <div className="App">
-      <Example />
+      <Flowfields />
     </div>
   );
 }
